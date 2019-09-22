e3ed5d768d408e09ccc70a6f49fb1c0a2abc03e2
Lucas Garron <lgarron@worldcubeassociation.org>
2019-09-21 18:43:58 -0700

    Change to exact URL matching.

--- lambda_function.rb
+++ lambda_function.rb
@@ -2,11 +2,11 @@ def parse_route(location)
     case location 
     when "/"
       "https://www.worldcubeassociation.org/"
-    when /([0-9][0-9][0-9][0-9][A-Z][A-Z][A-Z][A-Z][0-9][0-9])$/ #WCA ID
+    when /^\/([0-9][0-9][0-9][0-9][A-Z][A-Z][A-Z][A-Z][0-9][0-9])$/ #WCA ID
       "https://www.worldcubeassociation.org/persons/#{$1}"
-    when /([^\/]+)([0-9][0-9][0-9][0-9])$/ # Competition ID
+    when /^\/([^\/]+)([0-9][0-9][0-9][0-9])$/ # Competition ID
       "https://www.worldcubeassociation.org/competitions/#{$1}#{$2}"
-    when /\/(.*)$/
+    when /^\/(.*)$/
       "https://www.worldcubeassociation.org/#{$1}"
     end
 end
