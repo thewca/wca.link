450b4c6cafc8b8ef0aca0121f4b536317c732cff
Jeremy Fleischman <jeremyfleischman@gmail.com>
2019-09-21 17:56:20 -0700

    Some tweaks to actually get this working against the WCA aws account:
    We have successfully deployed this!
    
    1. We had to create the certificate in us-east-1.
       https://forums.aws.amazon.com/thread.jspa?messageID=770442 implies
       that this is necessary.
    2. Updated the code to actually create the hosted zone, because we
       didn't have one already.

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "us-east-1"
+}
+
 # The API Gateway
 resource "aws_api_gateway_rest_api" "wcalink_gateway" {
   name        = "WCAlink API Gateway"
@@ -5,39 +14,46 @@ resource "aws_api_gateway_rest_api" "wcalink_gateway" {
 }
 # Certificate for the https endpoint of the API Gateway
 resource "aws_acm_certificate" "cert" {
+  # Trying to use a certificate defined in us-west-2 gives the following error:
+  # Error: Error creating API Gateway Domain Name: BadRequestException: Invalid certificate ARN: arn:aws:acm:us-west-2:285938427530:certificate/eee73fc7-776b-4b26-a51c-e612f546ac39. Certificate must be in 'us-east-1'.
+  # status code: 400, request id: 6e1e2fb3-3d83-4899-8658-9efb105bedf1
+  # This looks like the same issue discussed here: https://forums.aws.amazon.com/thread.jspa?messageID=770442.
+  provider          = "aws.us-east-1"
+
   domain_name       = "wca.link"
   validation_method = "DNS"
 }
 # Route 53 Zone
-data "aws_route53_zone" "zone" {
+resource "aws_route53_zone" "zone" {
   name         = "wca.link."
-  private_zone = false
 }
 
 # Validate the Certificate with Route 53
 resource "aws_route53_record" "cert_validation" {
   name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
   type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.zone.id}"
+  zone_id = "${aws_route53_zone.zone.id}"
   records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
   ttl     = 60
+  allow_overwrite = true # Workaround for https://github.com/terraform-providers/terraform-provider-aws/issues/7918#issuecomment-503794355
 }
 
 resource "aws_acm_certificate_validation" "cert" {
+  provider = "aws.us-east-1" # The cert exists in us-east-1 (see comments above for aws_acm_certificate)
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
 # Custom Domain Name for the API Gateway Endpoint
 resource "aws_api_gateway_domain_name" "wcalink_domain" {
   certificate_arn = "${aws_acm_certificate_validation.cert.certificate_arn}"
-  domain_name     = "replaceme.ickler.cloud"
+  domain_name     = "wca.link"
 }
 
 # This is the Alias record for the API Gateway Domain Name
 resource "aws_route53_record" "alias" {
   name    = "${aws_api_gateway_domain_name.wcalink_domain.domain_name}"
   type    = "A"
-  zone_id = "${data.aws_route53_zone.zone.id}"
+  zone_id = "${aws_route53_zone.zone.id}"
 
   alias {
     evaluate_target_health = true
