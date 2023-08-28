7d55b31c51056b09ff23b6b41d52486204cd986a
Finn Ickler <finnickler@gmail.com>
2023-08-28 13:03:59 +0200

    updated ruby version

--- lambda.tf
+++ lambda.tf
@@ -23,72 +23,70 @@ EOF
 resource "aws_lambda_function" "wcalink_lambda" {
   filename      = "build/lambda_function_payload.zip"
   function_name = "wcalink_prod"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("build/lambda_function_payload.zip"))}"
-  source_code_hash = "${filebase64sha256("build/lambda_function_payload.zip")}"
+  source_code_hash = filebase64sha256("build/lambda_function_payload.zip")
 
-  runtime = "ruby2.7"
+  runtime = "ruby3.2"
 }
 # The Proxy Ressource Makes it possible for a whole path to be handled by
 # the same lambda
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.wcalink_gateway.id}"
-  parent_id   = "${aws_api_gateway_rest_api.wcalink_gateway.root_resource_id}"
+  rest_api_id = aws_api_gateway_rest_api.wcalink_gateway.id
+  parent_id   = aws_api_gateway_rest_api.wcalink_gateway.root_resource_id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.wcalink_gateway.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  rest_api_id   = aws_api_gateway_rest_api.wcalink_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 # Additional Route for the root
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.wcalink_gateway.id}"
-  resource_id   = "${aws_api_gateway_rest_api.wcalink_gateway.root_resource_id}"
+  rest_api_id   = aws_api_gateway_rest_api.wcalink_gateway.id
+  resource_id   = aws_api_gateway_rest_api.wcalink_gateway.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
 }
 # Plugging the Lambda behind the ressources
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.wcalink_gateway.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.wcalink_gateway.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.wcalink_lambda.invoke_arn}"
+  uri                     = aws_lambda_function.wcalink_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.wcalink_gateway.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.wcalink_gateway.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.wcalink_lambda.invoke_arn}"
+  uri                     = aws_lambda_function.wcalink_lambda.invoke_arn
 }
 # Deploying the API Gateway
 resource "aws_api_gateway_deployment" "wcalink_prod_deployment" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.wcalink_gateway.id}"
+  rest_api_id = aws_api_gateway_rest_api.wcalink_gateway.id
   stage_name  = "prod"
 }
 # Give the API Gateway Permission to invoke the Lambda
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.wcalink_lambda.function_name}"
+  function_name = aws_lambda_function.wcalink_lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
   # The "/*/*" portion grants access from any method on any resource
